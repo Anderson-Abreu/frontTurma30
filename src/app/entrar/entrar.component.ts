a43026f08ad177a0226350584f8acbaba88df604
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2021-10-14 10:53:25 -0300

    deploy

---
+++ src/app/entrar/entrar.component.ts
@@ -0,0 +1,52 @@
+import { environment } from './../../environments/environment.prod';
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
+import { AlertasService } from '../service/alertas.service';
+
+
+
+@Component({
+  selector: 'app-entrar',
+  templateUrl: './entrar.component.html',
+  styleUrls: ['./entrar.component.css']
+})
+export class EntrarComponent implements OnInit {
+
+  userLogin: UserLogin = new UserLogin()
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private alerta: AlertasService
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+  }
+
+  entrar() {
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+      environment.nome = this.userLogin.nome
+      environment.token = this.userLogin.token
+
+      console.log(environment.id)
+      console.log(environment.foto)
+      console.log(environment.nome)
+      console.log(environment.token)
+
+      this.router.navigate(['/inicio'])
+    }, erro => {
+      if (erro.status == 500) {
+        this.alerta.showAlertDanger('Usuário ou senha estão incorretos!')
+      }
+    })
+  }
+
+}
